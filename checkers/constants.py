42b05f804a9f6013e25c43bc3b46b585ccaaa6ec
Joao Antonio Goria Silva <jgoriasilva@gmail.com>
2023-09-23 21:33:40 +0200

    adding packages, constants to checkers game and base loop

---
+++ checkers/constants.py
@@ -0,0 +1,12 @@
+import pygame
+
+# board
+HEIGHT, WIDTH = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = HEIGHT//ROWS
+
+# rgb
+RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            
+            if event.type == pygame.QUIT:
+                run = False
+
+main()
+
