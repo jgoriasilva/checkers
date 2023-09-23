c358e360c82a089e08786015e23ebfedde2ed24e
Joao Antonio Goria Silva <jgoriasilva@gmail.com>
2023-09-23 21:56:10 +0200

    adding code to draw squares in board

---
+++ checkers/board.py
@@ -0,0 +1,11 @@
+from sqlite3 import SQLITE_IGNORE
+import pygame
+from .constants import BLACK, RED, ROWS, SQUARE_SIZE
+
+class Board():
+
+    def draw_squares(self, win: pygame.display):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row%2, 8, 2):
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
