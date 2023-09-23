86e86db2ee02306f30245a8ba3b887f3fb3cad4c
Joao Antonio Goria Silva <jgoriasilva@gmail.com>
2023-09-23 23:57:47 +0200

    added code to move piece by mouse click

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT, RED, WHITE
+from checkers.constants import SQUARE_SIZE, WIDTH, HEIGHT, RED, WHITE
 from checkers.board import Board
 from checkers.piece import Piece
 
@@ -8,14 +8,18 @@ FPS = 60
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
 def main():
     run = True
     clock = pygame.time.Clock()
     board = Board()
     board.create_pieces()
-
-    piece = board.get_piece(0,1)
-    board.move_piece(piece, 4, 3)
 
     while run:
         clock.tick(FPS)
@@ -23,6 +27,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move_piece(piece, 4, 3)
     
         board.draw_squares(win)
         board.draw_pieces(win)
